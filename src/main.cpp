71dd235a8231a70ab63c76edcb655a802412e868
Spiderman29 <francesco2.grassi@mail.polimi.it>
2024-12-13 10:55:16 +0100

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,26 @@ using namespace nbody;
 
 int main(){
 
-    Particle<double, 3> p1({0.0, 0.0, 10.0}, {0.0, 0.0, 0.0}, 1.0);
-    Particle<double, 3> p2({10.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, 1.0);
-    Particle<double, 3> p3({0.0, 10.0, 0.0}, {0.0, 0.0, 0.0}, 1.0);
+    Particle<double, 3> p1({0.0, 0.0, 10.0}, {0.0, 0.0, 0.0}, 200000000000000000.0);
+    Particle<double, 3> p2({10.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, 10000000.0);
+    Particle<double, 3> p3({0.0, 10.0, 0.0}, {0.0, 0.0, 0.0}, 90000.0);
+    Particle<double, 3> p4({0.0, 30.0, 0.0}, {0.0, 0.0, 0.0}, 90000000.0);
 
     nbody::Nbody<double, 3> nbody;
 
     nbody.addParticle(p1);
     nbody.addParticle(p2);
     nbody.addParticle(p3);
+    nbody.addParticle(p4);
 
-    for(int i = 0; i < 10; i++ ){
+    nbody.setDt(1.0);
+
+    for(int i = 0; i < 1000; i++ ){
         nbody.update();
         std::cout << nbody.particles[0].position[0] << " " << nbody.particles[0].position[1] << " " << nbody.particles[0].position[2] << std::endl;
         std::cout << nbody.particles[1].position[0] << " " << nbody.particles[1].position[1] << " " << nbody.particles[1].position[2] << std::endl;
         std::cout << nbody.particles[2].position[0] << " " << nbody.particles[2].position[1] << " " << nbody.particles[2].position[2] << std::endl;
+        std::cout << nbody.particles[3].position[0] << " " << nbody.particles[3].position[1] << " " << nbody.particles[3].position[2] << std::endl;
         std::cout << std::endl;
     }
 
--- src/nbody.hpp
+++ src/nbody.hpp
@@ -8,65 +8,69 @@
 
 namespace nbody {
 
-template <typename T, int D>
-class Particle {
-public:
-    std::array<T, D> position;
-    std::array<T, D> velocity;
-    T mass;
+    template <typename T, int D>
+    class Particle {
+    public:
+        std::array<T, D> position;
+        std::array<T, D> velocity;
+        T mass;
 
-    Particle(const std::array<T, D>& pos, const std::array<T, D>& vel, T m)
-        : position(pos), velocity(vel), mass(m) {}
-};
+        Particle(const std::array<T, D>& pos, const std::array<T, D>& vel, T m)
+            : position(pos), velocity(vel), mass(m) {}
+    };
 
-template<typename T, int DIM>
-class Nbody {
-public:
-    std::vector<std::vector<T>> forces;
-    std::vector<Particle<T, DIM>> particles;
-    T dt;
+    template<typename T, int DIM>
+    class Nbody {
+    public:
+        std::vector<std::vector<T>> forces;
+        std::vector<Particle<T, DIM>> particles;
+        T dt;
 
-    void addParticle(const Particle<T, DIM>& p) {
-        particles.push_back(p);
-        forces.push_back(std::vector<T>(DIM, 0.0));
-    }
-
-    void update() {
-        T distance = 0.0;
-        std::vector<T> diff(DIM, 0.0);
-
-        for (int q = 0; q < forces.size(); q++) {
-            forces[q] = std::vector<T>(DIM, 0.0);
+        void addParticle(const Particle<T, DIM>& p) {
+            particles.push_back(p);
+            forces.push_back(std::vector<T>(DIM, 0.0));
         }
 
-        for (int i = 0; i < particles.size(); i++) {
-            for (int j = i + 1; j < particles.size(); j++) {
-                for (int k = 0; k < DIM; k++) {
-                    diff[k] = particles[i].position[k] - particles[j].position[k];
-                }
+        void setDt(T d) {
+            dt = d;
+        }
 
-                distance = 0.0;
-                for (int k = 0; k < DIM; k++) {
-                    distance += diff[k] * diff[k];
-                }
-                distance = std::sqrt(distance);
+        void update() {
+            T distance = 0.0;
+            std::vector<T> diff(DIM, 0.0);
 
+            for (int q = 0; q < forces.size(); q++) {
+                forces[q] = std::vector<T>(DIM, 0.0);
+            }
+
+            for (int i = 0; i < particles.size(); i++) {
+                for (int j = i + 1; j < particles.size(); j++) {
+                    for (int k = 0; k < DIM; k++) {
+                        diff[k] = particles[i].position[k] - particles[j].position[k];
+                    }
+
+                    distance = 0.0;
+                    for (int k = 0; k < DIM; k++) {
+                        distance += diff[k] * diff[k];
+                    }
+                    distance = std::sqrt(distance);
+
+                    for (int k = 0; k < DIM; k++) {
+                        T force = G * particles[i].mass * particles[j].mass * diff[k] / std::pow(distance, 3);
+                        forces[i][k] += force;
+                        forces[j][k] -= force;
+                    }
+                }
+            }
+
+            for (int i = 0; i < particles.size(); i++) {
                 for (int k = 0; k < DIM; k++) {
-                    T force = G * particles[i].mass * particles[j].mass * diff[k] / std::pow(distance, 3);
-                    forces[i][k] += force;
-                    forces[j][k] -= force;
+                    particles[i].position[k] += particles[i].velocity[k] * dt;
+                    particles[i].velocity[k] += forces[i][k] * dt / particles[i].mass;
                 }
             }
         }
-
-        for (int i = 0; i < particles.size(); i++) {
-            for (int k = 0; k < DIM; k++) {
-                particles[i].position[k] += particles[i].velocity[k] * dt;
-                particles[i].velocity[k] += forces[i][k] * dt / particles[i].mass;
-            }
-        }
-    }
-};
+    };
 }
 
 #endif
