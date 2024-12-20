93727c13f6db439350cc8b02a29f3b218bf882f1
Ozard-02 <expoking2017@gmail.com>
2024-12-20 09:07:49 +0100

    orbiting

--- src/build/CMakeFiles/nbody.dir/DependInfo.cmake
+++ src/build/CMakeFiles/nbody.dir/DependInfo.cmake
@@ -8,7 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/c/Users/giorg/OneDrive/Documenti/GIORGIO/SCUOLA/UNIVERSITA/MAGISTRALE/AMSC/HANDSON/04-nbody-04-nbody/src/main.cpp" "CMakeFiles/nbody.dir/main.cpp.o" "gcc" "CMakeFiles/nbody.dir/main.cpp.o.d"
+  "/home/espo/uni/AMSC/04-nbody-04-nbody/src/main.cpp" "CMakeFiles/nbody.dir/main.cpp.o" "gcc" "CMakeFiles/nbody.dir/main.cpp.o.d"
+  "" "nbody" "gcc" "CMakeFiles/nbody.dir/link.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,39 @@
+
 #include "nbody.hpp"
-// #include "nbody_parallel.hpp"
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <chrono>
-#include <random>
 
 using namespace nbody;
 #define DIM 2
 
 int main() {
     std::string fileName="output.csv";
-    // std::string fileName="output_parallel.csv";
     std::ofstream outFile(fileName, std::ios::trunc);  
     if (!outFile.is_open()) {
         throw std::runtime_error("Impossibile aprire il file per la scrittura: output.csv");
     }
 
-    // Create a system with 1000 particles
+    Particle<double, DIM> p1({0.0, 0.0}, {0.0, 0.0}, 6.0e21);        // Massive central body
+    //Particle<double, DIM> p2({2500000.0, 5500000.0}, {-100000.0, 0.0}, 5.0e21);      // Orbiting body 1
+    //Particle<double, DIM> p3({-55000000.0, 500000.0}, {-0.7, 300000.0}, 2.0e21);     // Orbiting body 2
+    Particle<double, DIM> p4({0.0, 384.0e6}, {102200000, 0.0}, 7.0e22);   // Diagonal trajectory
+
     nbody::Nbody<double, DIM> nbody;
-    double time_step = 0.1;
-    
-    Particle<double, DIM> p1({0.0, 0.0}, {2000.0, 0.0}, 5.0e21);        // Massive central body
-    Particle<double, DIM> p2({25000.0, 55000.0}, {-2000.0, 0.0}, 5.0e21);      // Orbiting body 1
-    Particle<double, DIM> p3({55000.0, 500.0}, {-0.7, 3000.0}, 2.0e21);     // Orbiting body 2
-    //Particle<double, DIM> p4({0.0, 10.0}, {0.5, -0.5}, 5.0e9);   // Diagonal trajectory
+    double time_step=0.01;
 
-
-    // // Initialize random number generator
-    // std::random_device rd;
-    // std::mt19937 gen(rd());
-    // std::uniform_real_distribution<> pos_dist(-10.0, 10.0);  // Position distribution
-    // std::uniform_real_distribution<> vel_dist(-0.5, 0.5);    // Velocity distribution
-    // std::uniform_real_distribution<> mass_dist(0.1, 1.0);    // Mass distribution
-
-    // // Generate 1000 particles with random positions, velocities, and masses
-    // for (int i = 0; i < 400; ++i) {
-    //     std::array<double, DIM> position;
-    //     std::array<double, DIM> velocity;
-        
-    //     // Generate random position and velocity components
-    //     for (int d = 0; d < DIM; ++d) {
-    //         position[d] = pos_dist(gen);
-    //         velocity[d] = vel_dist(gen);
-    //     }
-        
-    //     double mass = mass_dist(gen);
-    //     Particle<double, DIM> p(position, velocity, mass);
-    //     nbody.addParticle(p);
-    // }
+    nbody.addParticle(p1);
+    //nbody.addParticle(p2);
+    //nbody.addParticle(p3);
+    nbody.addParticle(p4);
 
     nbody.setDt(time_step);
 
     auto start = std::chrono::high_resolution_clock::now();
     
     double currentTime = 0.0;
-    double simulationTime = 20.0;
+    double simulationTime = 1.0;  // 10 seconds
     while (currentTime < simulationTime) {
         nbody.update();
         nbody.exportToCsv(fileName);
